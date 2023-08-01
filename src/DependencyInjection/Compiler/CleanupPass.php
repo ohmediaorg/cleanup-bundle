5a18def0692065cf56f88b1bc5dbbcca9018651a
Justin Hoffman <justin@ohmedia.ca>
2023-08-01 15:19:18 -0600

    php-cs-fixer using @Symfony rules

--- src/DependencyInjection/Compiler/CleanupPass.php
+++ src/DependencyInjection/Compiler/CleanupPass.php
@@ -2,8 +2,8 @@
 
 namespace OHMedia\CleanupBundle\DependencyInjection\Compiler;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
 class CleanupPass implements CompilerPassInterface
--- src/DependencyInjection/OHMediaCleanupExtension.php
+++ src/DependencyInjection/OHMediaCleanupExtension.php
@@ -9,15 +9,12 @@ use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
- * This is the class that loads and manages your bundle configuration
+ * This is the class that loads and manages your bundle configuration.
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
 class OHMediaCleanupExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
