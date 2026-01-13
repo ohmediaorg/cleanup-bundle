bbe7e98cbbdc2c938d4363d5e72a18e6d142b0aa
Justin Hoffman <justin@ohmedia.ca>
2026-01-13 16:43:18 -0600

    ditch alias

--- src/DependencyInjection/Compiler/CleanupPass.php
+++ src/DependencyInjection/Compiler/CleanupPass.php
@@ -2,6 +2,7 @@
 
 namespace OHMedia\CleanupBundle\DependencyInjection\Compiler;
 
+use OHMedia\CleanupBundle\Command\CleanupCommand;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -11,11 +12,11 @@ class CleanupPass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         // always first check if the primary service is defined
-        if (!$container->has('oh_media_cleanup.command')) {
+        if (!$container->has(CleanupCommand::class)) {
             return;
         }
 
-        $definition = $container->findDefinition('oh_media_cleanup.command');
+        $definition = $container->findDefinition(CleanupCommand::class);
 
         $tagged = $container->findTaggedServiceIds('oh_media_cleanup.cleaner');
 
